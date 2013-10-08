bf12e21158de0b6bbfb28b011098deb27e3df82e
Zoran Zaric <zz@zoranzaric.de>
2013-10-08 15:36:02 +0200

    Add basic webapp

---
+++ webapp/brieftraeger/admin.py
@@ -0,0 +1,5 @@
+from brieftraeger.models import List, Subscriber
+from django.contrib import admin
+
+admin.site.register(List)
+admin.site.register(Subscriber)
---
+++ webapp/brieftraeger/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.sites.models import Site
+
+class List(models.Model):
+    name = models.CharField(max_length=200)
+    site = models.ForeignKey(Site)
+
+    def __unicode__(self):
+        return "%s.%s" % (self.name, self.site.domain)
+
+class Subscriber(models.Model):
+    email = models.EmailField()
+    list = models.ForeignKey(List)
+
+    def __unicode__(self):
+        return self.email
+
+class Email(models.Model):
+    message_id = models.CharField(max_length=255)
+    list = models.ForeignKey(List, null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True)
+    message = models.TextField()
+
